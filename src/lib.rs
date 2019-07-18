24e951571180c352ee485919d45f6d3f802e8236
kaikai1024 <kaikaifeng1024@gmail.com>
2019-07-18 15:00:04 +0800

    Directly public use.

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,8 @@ pub(crate) mod test;
 
 #[macro_use]
 extern crate cita_logger as logger;
+
+pub use self::columns::NUM_COLUMNS;
+pub use self::config::Config;
+pub use self::database::{Database, DataCategory};
+pub use self::rocksdb::RocksDB;
