8786258d40b826d919823494bb6146d0c358dac2
kaikai1024 <kaikaifeng1024@gmail.com>
2019-07-31 17:05:57 +0800

    New `Result` type.

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,8 @@
 use crate::error::DatabaseError;
 use rocksdb::DBIterator;
+use std::result;
+
+pub type Result<T> = result::Result<T, DatabaseError>;
 
 /// Specify the category of data stored, and users can store the data in a
 /// decentralized manner.
@@ -22,43 +25,30 @@ pub enum DataCategory {
 }
 
 pub trait Database: Send + Sync {
-    fn get(
-        &self,
-        category: Option<DataCategory>,
-        key: &[u8],
-    ) -> Result<Option<Vec<u8>>, DatabaseError>;
+    fn get(&self, category: Option<DataCategory>, key: &[u8]) -> Result<Option<Vec<u8>>>;
 
     fn get_batch(
         &self,
         category: Option<DataCategory>,
         keys: &[Vec<u8>],
-    ) -> Result<Vec<Option<Vec<u8>>>, DatabaseError>;
+    ) -> Result<Vec<Option<Vec<u8>>>>;
 
-    fn insert(
-        &self,
-        category: Option<DataCategory>,
-        key: Vec<u8>,
-        value: Vec<u8>,
-    ) -> Result<(), DatabaseError>;
+    fn insert(&self, category: Option<DataCategory>, key: Vec<u8>, value: Vec<u8>) -> Result<()>;
 
     fn insert_batch(
         &self,
         category: Option<DataCategory>,
         keys: Vec<Vec<u8>>,
         values: Vec<Vec<u8>>,
-    ) -> Result<(), DatabaseError>;
+    ) -> Result<()>;
 
-    fn contains(&self, category: Option<DataCategory>, key: &[u8]) -> Result<bool, DatabaseError>;
+    fn contains(&self, category: Option<DataCategory>, key: &[u8]) -> Result<bool>;
 
-    fn remove(&self, category: Option<DataCategory>, key: &[u8]) -> Result<(), DatabaseError>;
+    fn remove(&self, category: Option<DataCategory>, key: &[u8]) -> Result<()>;
 
-    fn remove_batch(
-        &self,
-        category: Option<DataCategory>,
-        keys: &[Vec<u8>],
-    ) -> Result<(), DatabaseError>;
+    fn remove_batch(&self, category: Option<DataCategory>, keys: &[Vec<u8>]) -> Result<()>;
 
-    fn restore(&mut self, new_db: &str) -> Result<(), DatabaseError>;
+    fn restore(&mut self, new_db: &str) -> Result<()>;
 
     fn iterator(&self, category: Option<DataCategory>) -> Option<DBIterator>;
 
